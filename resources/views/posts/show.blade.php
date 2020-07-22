7673d0d70fa291bd43dc5575698c6ca21bf0a496
islamAwaad <islamawaad777@outlook.com>
2020-07-22 09:59:49 +0200

    add created at and by to show posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,10 +1,22 @@
 @extends('index')
 @section('content')
 <div class="mt-5">
-    <h1>{{$post->title}}</h1>
+    <div>
+        <h1 style="display:inline">{{$post->title}}</h1>
+        <p class="float-right">Created At {{$post->created_at}}</p>
+    </div>
     <img class="mt-2 mb-5 offset-2" src="{{$post->image}}" alt="">
     <div class="mb-5">
         {!! $post->description !!}
+    </div>
+    <div>
+        <p class="float-right">Created By
+            @if(Auth()->guest())
+            {{$post->user->name}}
+            @else
+            {{Auth()->user()->id == $post->user->id ? 'me': $post->user->name}}
+            @endif
+        </p>
     </div>
     <a href="{{url()->previous()}}" class="mb-5 btn btn-info">
         back
